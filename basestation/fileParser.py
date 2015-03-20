20b03cedb3414b7ddaaa0d917f2fac0b11227b16
Dan Becker <dabecker@paypal.com>
2015-03-20 09:25:56 -0500

    Put datetime with data samples

--- basestation/fileParser.py
+++ basestation/fileParser.py
@@ -19,15 +19,14 @@ import re
 lineFilter = '^\s*R\[.+'
 lineFilterRE = re.compile(lineFilter)
 fields= '^\s*R\[(?P<packet>\d+)\]\[(?P<device>\d+)\]\s*\"(?P<properties>[^\"]*)\" - ack ts=(?P<timestamp>.*).*'
+timeStampRE = '((1\d{3})|(20\d{2}))-((0\d)|(1[0-2]))-(([1-2]\d)|(3[0-1])) (([0-1]\d)|(2[0-3])):([0-5]\d):([0-5]\d)'
 fieldsRE = re.compile( fields )
-propDelim = ','
-keyValDelim = '='
 keyAggr = ['tid','sid']
 
 # Returns a dict organized by given data keys
 # For example, consider a file along these lines.
-#    R[1][2] "tid=t1, sid=s0, tf=68.1, hp=72, vvr=5.44, tm=594" - ack now=2015-03-19 09:51:45.
-#    R[2][2] "tid=t1, sid=s1, tf=67.2, pia=29.03, pir=29.94" - ack now=2015-03-19 09:51:55
+#    R[1][2] "tid=t1, sid=s0, tf=68.1, hp=72, vvr=5.44, tm=594" - ack ts=2015-03-19 09:51:45.
+#    R[2][2] "tid=t1, sid=s1, tf=67.2, pia=29.03, pir=29.94" - ack ts=2015-03-19 09:51:55.
 # Results
 # dict key  val       
 # t1.s0.tf  [68.1,69.2,70.2,71.0]
@@ -37,49 +36,59 @@ def parseFile(filename):
   values = {}  # Map each word to its count
   input_file = open(filename, 'r')
   for line in input_file:
+    # Filter out candidate lines with an easy RE.
     lineMatch = lineFilterRE.match(line)
     if lineMatch:
-      # print line
+      # Match on packet, devvice, properties, timestamp group
       groups = fieldsRE.match(line)
       if groups:
          packet = groups.group('packet')
          timestamp = groups.group('timestamp')
          properties = groups.group('properties')
          print 'packet,time,props=' + packet + ',' + timestamp + ',' + properties
+         # Break properties into keys,values list
          words = re.findall(r"[\w\.]+", properties )
          # print 'words=' + str( words )
          keyComp = ''
+         # Go through list two at a time.
          for i  in range( 0, len( words ), 2 ):
             key = words[ i ] 
             value = words[ i + 1 ] 
             if key in keyAggr:
+                # Build a dot delimited compound key out of the values.
                 if len( keyComp ) > 0:
                     keyComp += '.'
                 keyComp += value
             else:
+                # Build a dot delimited compund key with this key.
                 # print 'keyComp,value=' + keyComp + '.' + key + '=' + value
                 keyCurr = keyComp + '.' + key
                 if keyCurr in values:
+                    # Add a (time,value) tuple to this list.
                     currValue = values[ keyCurr ]
-                    currValue.append( value )
+                    currValue.append( (timestamp,value) )
                     values[ keyCurr ] = currValue
                 else:
-                    values[ keyCurr ] = [ value ] 
+                    # Create a (time,value) tuple list.
+                    values[ keyCurr ] = [ (timestamp, value) ] 
   input_file.close()  # Not strictly required, but good form.
   return values
 
 # Given two timestamps, makes wild cards of dissimilar fields
-# For example, 2015-03-15 10:31:22 and 2015-03-15 10:32:05 yields 2015-03-15 10:xx:xx 
-def timeStampWild(ts1,ts2,replaceChar='X'):
+# Once a wild card occurs, all following digits have wild cards
+# For example, 2015-03-15 10:31:25 and 2015-03-15 10:32:05 yields 2015-03-15 10:3X:XX 
+def timeStampWild(ts1,ts2,wildChar='X'):
     delimiters = [ '-', ':', '/' ]
+    goneWild = False
     ts = ''
     for t1,t2 in zip(ts1,ts2):
         if t1 in delimiters:
             ts += t1
-        elif (t1 == t2):
-            ts += t1 
+        elif (t1 != t2) or goneWild:
+            ts += wildChar 
+            goneWild = True
         else:
-            ts += replaceChar 
+            ts += t1 
     return ts
  
 # Basic command line argument parsing code
@@ -98,7 +107,10 @@ def main():
   else:
     print 'unknown option: ' + option
     sys.exit(1)
-  print timeStampWild( '2015-03-15 10:31:04', '2015/03/15 10:32:44')
+    
+  t1 = '2015-03-15 10:31:04'
+  t2 = '2015/03/15 10:32:44'
+  print 't1 (' + t1 + ') or t2(' + t2 + ') => ' + timeStampWild( t1, t2 )
 
 if __name__ == '__main__':
   main()
